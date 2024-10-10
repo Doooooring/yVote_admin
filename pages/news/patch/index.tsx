0ba1e87206ea1f76c33055c659e13a89c0bb72bb
kang seok <sekeipost@naver.com>
2024-10-10 22:02:50 +0900

    chore

--- pages/news/patch/index.tsx
+++ pages/news/patch/index.tsx
@@ -56,7 +56,6 @@ export default function NewsPatch({ data }: pageProps) {
 
   const [id, setId] = useState<string>('');
   const [title, setTitle] = useState<string>('');
-  const [summary, setSummary] = useState<string>('');
   const [timeline, setTimeline] = useState<News['timeline']>([]);
   const [state, setState] = useState<boolean>(true);
   const [isPublished, setIsPublished] = useState<boolean>(true);
