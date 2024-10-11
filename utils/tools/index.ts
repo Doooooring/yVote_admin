02a8fde1d57c92c92e45f6f6f4c6a20cfaa729c6
doring <2017132021@yonsei.ac.kr>
2024-10-11 14:55:39 +0900

    add timeout util

--- utils/tools/index.ts
+++ utils/tools/index.ts
@@ -8,3 +8,28 @@ export const htmlFromString = (s: string) => {
 export const sortKorCallback = (a: string, b: string) => {
   return a > b ? 1 : a < b ? -1 : 0;
 };
+
+export const throttle = <T>(cb: (...args: T[]) => any, sec: number) => {
+  let timer: NodeJS.Timeout | null = null;
+  return (...arg: T[]) => {
+    if (timer) return;
+    timer = setTimeout(() => {
+      cb(...arg);
+      timer = null;
+    }, sec);
+  };
+};
+
+type commFunc = <T>(...args: T[]) => any;
+
+export const debounce = <T>(cb: (...args: T[]) => any, sec: number) => {
+  let timer: NodeJS.Timeout | null = null;
+  return (...arg: T[]) => {
+    if (timer) {
+      clearTimeout(timer);
+    }
+    timer = setTimeout(() => {
+      cb(...arg);
+    }, sec);
+  };
+};
